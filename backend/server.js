0072b1ddc5e460edf6b22f3baaabe07d484a6f83
shaik Asif <smd.20sa@gmail.com>
2023-10-03 23:59:57 +0530

    register user in controllers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+
 dotenv.config();
+connectDB();
 const app = express();
+app.use("/api/user", userRoutes);
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, console.log("server started on port 4000"));
+app.listen(PORT, console.log("server started on port 4000".yellow.bold));
