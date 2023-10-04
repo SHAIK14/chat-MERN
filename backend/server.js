cc76d83c8b243b2deae33deea8ff06b69e46abc1
shaik Asif <smd.20sa@gmail.com>
2023-10-04 17:16:08 +0530

    now the mongo works fine

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json());
 app.use("/api/user", userRoutes);
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log("server started on port 4000".yellow.bold));
