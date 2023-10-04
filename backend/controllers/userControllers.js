c24365bf57674c1f84e82060bf33b6a799013ff4
shaik Asif <smd.20sa@gmail.com>
2023-10-04 23:59:05 +0530

    done with signup  and login in postman

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -35,5 +35,24 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Failed to create a user");
   }
 });
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-module.exports = { registerUser };
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+module.exports = { registerUser, authUser };
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 app.use("/api/user", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log("server started on port 4000".yellow.bold));
