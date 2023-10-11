e47552ae340740fe657b09e58b04261e55e56a92
shaik Asif <smd.20sa@gmail.com>
2023-10-11 16:48:34 +0530

    message sending

--- backend/Models/chatModel.js
+++ backend/Models/chatModel.js
@@ -1,14 +1,24 @@
 const mongoose = require("mongoose");
 
-const chatModel = mongoose.Schema(
-  {
-    chatName: { type: String, trim: true },
-    isGroupChat: { type: Boolean, default: false },
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
-    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  },
-  { timestamps: true }
-);
-const Chat = mongoose.model("Chat", chatModel);
+let Chat;
+
+if (mongoose.models.Chat) {
+  // Model already exists, use the existing one
+  Chat = mongoose.model("Chat");
+} else {
+  // Model doesn't exist, define and compile it
+  const chatModel = mongoose.Schema(
+    {
+      chatName: { type: String, trim: true },
+      isGroupChat: { type: Boolean, default: false },
+      users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+      groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    },
+    { timestamps: true }
+  );
+
+  Chat = mongoose.model("Chat", chatModel);
+}
+
 module.exports = Chat;
--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -1,39 +1,79 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema(
-  {
-    name: { type: "String", required: true },
-    email: { type: "String", unique: true, required: true },
-    password: { type: "String", required: true },
-    pic: {
-      type: "String",
+let User;
 
+if (mongoose.models.User) {
+  // Model already exists, use the existing one
+  User = mongoose.model("User");
+} else {
+  // Model doesn't exist, define and compile it
+  const userSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
-    // isAdmin: {
-    //   type: Boolean,
-    //   required: true,
-    //   default: false,
-    // },
-  },
-  { timestaps: true }
-);
+  });
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
+  userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified) {
+  userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
     next();
-  }
+  });
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
-
-const User = mongoose.model("User", userSchema);
+  User = mongoose.model("User", userSchema);
+}
 
 module.exports = User;
+
+// const mongoose = require("mongoose");
+// const bcrypt = require("bcryptjs");
+
+// const userSchema = mongoose.Schema(
+//   {
+//     name: { type: "String", required: true },
+//     email: { type: "String", unique: true, required: true },
+//     password: { type: "String", required: true },
+//     pic: {
+//       type: "String",
+
+//       default:
+//         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+//     },
+//     // isAdmin: {
+//     //   type: Boolean,
+//     //   required: true,
+//     //   default: false,
+//     // },
+//   },
+//   { timestaps: true }
+// );
+
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified) {
+//     next();
+//   }
+
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+// });
+
+// const User = mongoose.model("User", userSchema);
+
+// module.exports = User;
